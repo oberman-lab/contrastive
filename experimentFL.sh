88546a8a7e9e76a370c367f9b0ac3642b1cb1d6c
nmarzz <ndmarshall97@gmail.com>
2020-10-20 12:46:05 -0400

    add bash experiment

---
+++ experimentFL.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+# An script to train LeNet on MNIST with SGD
+
+LOGDIR='./logs/'
+DATADIR='./data/'
+mkdir -p $LOGDIR
+mkdir -p $DATADIR
+GPUID=0 # Select a GPU. If you want say two GPUs, set GPUID="0,1"
+
+CUDA_VISIBLE_DEVICES=$GPUID \
+
+for VARIABLE in 0.01 0.5 0.1 0.2 0.3 0.4 0.5
+do
+
+python -u ../main.py\
+  --data $DATADIR\
+  --dropout 0.25\
+  --momentum 0.9\
+  --compare True \
+  --frac_labeled $VARIABLE \
+  --epochs 10 > $LOGDIR/log.out
+
+done
