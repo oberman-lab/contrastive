70da3cf436839b8923d28bf5ad082352418d611e
nmarzz <ndmarshall97@gmail.com>
2020-10-29 11:49:45 -0400

    organizing

---
+++ semisupervised/procedures.py
@@ -0,0 +1,71 @@
+import torch
+import torchnet as tnt
+from semisupervised.data_processing.utils import cycle_with
+from semisupervised.losses.helpers import returnClosestCenter
+
+
+
+def run_epoch(model, current_epoch, data_loaders, optimizer, device, args,loss_function ,writer):
+    model.train()
+    # We loop over all batches in the (bigger) unlabeled set. While we do so we loop also on the labeled data, starting over if necessary.
+    # This means that unlabeled data may be present many times in the same epoch.
+    # The number of times that the labeled data is processed is dependent on the batch size of the labeled data.
+    # for batch_ix, unlabeled_features in enumerate(data_loaders['unlabeled']):
+    for batch_ix, (unlabeled_features, (labeled_features, labels)) in enumerate(
+            cycle_with(leader=data_loaders['unlabeled'], follower=data_loaders['labeled'])):
+
+        unlabeled_features = unlabeled_features.to(device)
+        labeled_features = labeled_features.to(device)
+        labels = labels.to(device)
+
+        labeled_output = model(labeled_features)
+        unlabeled_output = model(unlabeled_features)
+        loss = loss_function(unlabeled_output, labeled_output, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch_ix % args.log_interval == 0 and batch_ix > 0:
+            print('Semi Supervised: [Epoch %2d, batch %3d] training loss: %.4f' %
+                  (current_epoch, batch_ix, loss))
+    if writer is not None:
+        writer.add_scalar('train/loss/semi', loss,current_epoch)
+
+def train_supervised(model,current_epoch,data_loaders,optimizer,device,args,loss_function,writer = None):
+    model.train()
+
+    for batch_ix,(data,labels) in enumerate(data_loaders['labeled']):
+        data = data.to(device)
+        labels = labels.to(device)
+
+        output = model(data)
+        loss = loss_function(output,labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch_ix % (args.log_interval//5) == 0 and batch_ix > 0:
+            print('Fully Supervised: [Epoch %2d, batch %3d] training loss: %.4f' %
+                  (current_epoch, batch_ix, loss))
+    if writer is not None:
+        writer.add_scalar('train/loss/fully', loss,current_epoch)
+
+def test_model(model,current_epoch, data_loaders, loss_function,centers, device,writer):
+    model.eval()
+    top1 = tnt.meter.ClassErrorMeter(accuracy = True)
+    test_loss = tnt.meter.AverageValueMeter()
+    with torch.no_grad():
+        for data, target in data_loaders['test']:
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+
+            top1.add(-torch.cdist(output,centers),torch.argmax(target,dim = 1))
+            loss = loss_function(output, target)
+            test_loss.add(loss.cpu())
+
+    print('[Epoch %2d] Average test loss: %.5f, Accuracy: %.5f'
+          % (current_epoch, test_loss.value()[0], top1.value()[0]))
+    if writer is not None:
+        writer.add_scalar('test/loss',  test_loss.value()[0],current_epoch)
+        writer.add_scalar('test/acc', top1.value()[0],current_epoch)
---
+++ semisupervised/test_.py
@@ -0,0 +1,98 @@
+import torch
+import argparse
+import pytest
+import traceback
+from arg_parser import ContrastiveArgParser
+from data_processing.contrastive_data import *
+
+''' Install pytest - navigate to this directory - call pytest - pray nothing broke'''
+
+parser = ContrastiveArgParser()
+args = parser.parse_args()
+args.cuda =False #False until proven otherwise
+
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    args.cuda = True
+kwargs = {'num_workers': 0, 'pin_memory': True} if args.cuda else {}
+
+
+''' Testing functions below here '''
+
+def test_labelsStrippedMNIST():
+    '''If the labels are stripped, the first element '''
+    dataLoaders = ContrastiveData(args.frac_labeled, args.data_dir, args.batch_size_labeled, args.batch_size_unlabeled,
+                                  dataset_name='Projection', **kwargs).get_data_loaders()
+    test = iter(dataLoaders['unlabeled'])
+    assert (type(next(test)) is not list), 'The labels were not stripped off'
+
+
+def test_loadMNIST():
+    try:
+        dataLoaders = ContrastiveData(args.frac_labeled, args.data_dir, args.batch_size_labeled,
+                                      args.batch_size_unlabeled, dataset_name='MNIST', **kwargs).get_data_loaders()
+
+        for i,(data,label) in enumerate(dataLoaders['labeled']):
+            print(label)
+
+    except Exception:
+        print(traceback.print_exc())
+        pytest.fail("Loading MNIST failed")
+
+
+def test_loadFashionMNIST():
+    try:
+        dataLoaders = ContrastiveData(args.frac_labeled,args.data_dir,args.batch_size_labeled,args.batch_size_unlabeled,dataset_name = 'FashionMNIST', **kwargs).get_data_loaders()
+
+        for i,(data,label) in enumerate(dataLoaders['labeled']):
+            print(label)
+    except Exception:
+        print(traceback.print_exc())
+        pytest.fail("Loading Fashion MNIST failed")
+
+def test_CIFAR10():
+    try:
+        dataLoaders = ContrastiveData(args.frac_labeled,args.data_dir,args.batch_size_labeled,args.batch_size_unlabeled,dataset_name = 'CIFAR10', **kwargs).get_data_loaders()
+        for i,(data,label) in enumerate(dataLoaders['labeled']):
+            print(label)
+    except Exception:
+        print(traceback.print_exc())
+        pytest.fail("Loading Fashion MNIST failed")
+
+
+def test_loadProjection():
+    try:
+        dataLoaders = ContrastiveData(args.frac_labeled, args.data_dir, args.batch_size_labeled,
+                                      args.batch_size_unlabeled, dataset_name='Projection', **kwargs).get_data_loaders()
+    except Exception:
+        print(traceback.print_exc())
+        pytest.fail("Loading ProjectionDataset failed")
+
+
+def test_badData():
+    with pytest.raises(ValueError) as context:
+        dataLoaders = ContrastiveData(args.frac_labeled, args.data_dir, args.batch_size_labeled,
+                                      args.batch_size_unlabeled, dataset_name='!@##$$)*^!^@##@****!!@GDTGENOTADATASET',
+                                      **kwargs)
+    assert "Dataset name is not supported" in str(context)
+
+
+def test_genProjectionData():
+    import shutil  # Don't want this to have access outside of this function
+    path = 'tempDirectoryForTesting'
+    data = ProjectionData(path, train=True, num_clusters=10)
+    item, label = data.__getitem__(0)
+    shutil.rmtree(path)  # Removes directory to force data generation
+    assert label.size() == item.size(), 'Couldn\'t generate ProjectionData'
+
+
+def test_cycle_with():
+    from data_processing.utils import cycle_with
+    unlabeled = [1, 2, 3, 4, 5, 6, 7]
+    labeled = [1, 2, 3]
+    result = [(1, 1), (2, 2), (3, 3), (4, 1), (5, 2), (6, 3), (7, 1)]
+    check = []
+    for elt in cycle_with(unlabeled, labeled):
+        check.append(elt)
+    assert check == result, "Iterator not working"
